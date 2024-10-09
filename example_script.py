135e5c45d811f83184b4f17fbfb03ebab3c425c7
renatacastellanos <rcastellanos@wvstateu.edu>
2024-10-09 10:02:03 -0400

    Add files via upload

---
+++ example_script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 09:51:26 2024
+
+@author: renatacastellanos
+"""
+
+print("Hello, World!")
+input("Press Enter to exit...")
